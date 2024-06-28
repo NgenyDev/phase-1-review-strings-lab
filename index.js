865a91d1f91b33a1ddf1cb1da500aa1c93c4984e
Ngeny Gilbert <ngenygkip@gmail.com>
2024-06-28 14:34:39 +0300

    strings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const currentUser = `Grace Hopper`;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, G!`
+const shortGreeting = `Welcome, ${currentUser[0]}!`
 
 
